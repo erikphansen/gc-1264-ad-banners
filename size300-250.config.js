d44925e1f4cbabcf435e286d44d180a383216b7a
Erik P. Hansen <eph@erikphansen.com>
2021-04-04 12:03:08 -0700

    reorg

--- size300-250.config.js
+++ size300-250.config.js
@@ -1,58 +1,6 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpackConfig = require('./webpackConfig')
 
-const adSize = {
+module.exports = webpackConfig({
   width: 300,
   height: 250,
-}
-
-module.exports = {
-  mode: 'development',
-  entry: './src/300x250.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: `${adSize.width}x${adSize.height}`,
-      template: `src/index_${adSize.width}x${adSize.height}.html`,
-      filename: `${adSize.width}x${adSize.height}.html`,
-      inject: false,
-      cache: false,
-    }),
-  ],
-  output: {
-    // makes a new bundle for each entry
-    filename: '[name].bundle.js',
-    // path of the output file
-    path: path.resolve(__dirname, `dist/${adSize.width}x${adSize.height}`),
-    clean: true,
-    publicPath: '',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-}
+})
--- src/300x250/ad.js
+++ src/300x250/ad.js
@@ -1,12 +1,20 @@
 import gsap from 'gsap'
 
-import './sass/300x250.scss'
-import sketchbook1 from './images/300x250/sketchbook-1.png'
-import sketchbook2 from './images/300x250/sketchbook-2.png'
-import sketchbook3 from './images/300x250/sketchbook-3.png'
-import sketchbook4 from './images/300x250/sketchbook-4.png'
-import sketchbook5 from './images/300x250/sketchbook-5.png'
-import sketchbook6 from './images/300x250/sketchbook-6.png'
+import '../sass/300x250.scss'
+import sketchbook1 from './images/sketchbook-1.png'
+import sketchbook2 from './images/sketchbook-2.png'
+import sketchbook3 from './images/sketchbook-3.png'
+import sketchbook4 from './images/sketchbook-4.png'
+import sketchbook5 from './images/sketchbook-5.png'
+import sketchbook6 from './images/sketchbook-6.png'
+import line1 from './images/1-a-new-line.png'
+import line2 from './images/2-of-art-pads.png'
+import line3 from './images/3-for-a-new.png'
+import line4 from './images/4-generation.png'
+import line5 from './images/5-of-artists.png'
+import line6 from './images/6-discover-yourself.png'
+import logo from './images/logo.png'
+import button from './images/button.png'
 
 const ad = document.querySelector('#ad')
 
